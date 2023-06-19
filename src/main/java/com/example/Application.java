7f8d39665a549d2cad72a5897d1c237e018c6480
protocols <protocols@example.com>
2023-06-19 12:04:13 +0900

    Application 수정

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		
+		System.out.println("#######################");
+		System.out.println("Spring Boot Start");
+		System.out.println("#######################");
 		SpringApplication.run(Application.class, args);
+		
 	}
 
 }
